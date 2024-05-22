d1e328b26e15158552b95dbc74a3a15016b7f709
Walid Guetat <63478267+walidguetat@users.noreply.github.com>
2024-05-22 23:29:41 +0100

    Function GetMessage()

---
+++ Algorithms-and-Problem-Solving-C-/Course 1/Prblm 1/GetMessage.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+#include<String>
+using namespace std;
+// create function GetMessage is print message 'welcome in c++'
+void GetMessage() {
+    cout << "Welcome in c++ ! " << endl;
+}
+int main()
+{
+    // call this function in main() 
+    GetMessage();
+    return 0;
+}
---
+++ Course 1/Prblm 1/prblm1.cpp
@@ -0,0 +1,6 @@
+# include <iostream>
+using namespace std;
+int main(){
+    cout<<"hello walid how are you ?"<<endl;
+    return 0;
+}
